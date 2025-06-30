5e536a537a7055d69a45586d99b860aaed48faa8
Ismaël Bouya <ismael.bouya@novainsilico.ai>
2025-06-30 10:41:50 +0200

    Add a jinko-env-big

--- flake.nix
+++ flake.nix
@@ -33,42 +33,52 @@
             export POETRY_CACHE_DIR="$(pwd)/.cache/pypoetry"
             source .envrc
           '';
+          mkJinkoEnv = env_path: name: dream2nix.lib.evalModules {
+            packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+              dream2nix.modules.dream2nix.WIP-python-pyproject
+              ({ config, ... }: {
+                deps = {nixpkgs, ...}: {
+                  python = nixpkgs.python312;
+                  poetry = nixpkgs.poetry;
+                  patchelf = nixpkgs.patchelf;
+                  rdma-core = nixpkgs.rdma-core;
+                };
+
+                public.pythonWithEnv = config.deps.python.withPackages (_: config.mkDerivation.propagatedBuildInputs);
+                mkDerivation.src = env_path;
+                paths.projectRoot = ./.;
+                paths.package = name;
+                pip.overrides.nvidia-cufile-cu12.mkDerivation.buildInputs = [
+                  config.deps.rdma-core
+                ];
+                # There are some collisions between this and nvidia-nvtx-cu12
+                pip.overrides.nvidia-cufile-cu12.mkDerivation.postFixup = ''
+                  rm -rf $out/lib/python*/site-packages/nvidia/__pycache__
+                '';
+                pip.overrides.kaleido.mkDerivation.postFixup = ''
+                  sed -i -e "s@/bin/bash@/bin/sh@" $out/lib/python*/site-packages/kaleido/executable/kaleido
+                  ${config.deps.patchelf}/bin/patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/lib/python*/site-packages/kaleido/executable/bin/kaleido
+                '';
+                # Not sure how to properly build a folder, so we turn
+                # it into a whl file which is the known case
+                pip.overrides.jinko-sdk.mkDerivation.src = let
+                  pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+                  filename = "jinko_sdk-${pyproject.tool.poetry.version}-py${config.deps.python.sourceVersion.major}-none-any.whl";
+                in pkgs.runCommand filename {} ''
+                  HOME=$(pwd)
+                  cd ${./.}
+                  ${config.deps.poetry}/bin/poetry build -o $HOME/dist
+                  cp $HOME/dist/${filename} $out
+                '';
+              })
+            ];
+          };
         in
         {
           packages = {
-            jinkoEnv = dream2nix.lib.evalModules {
-              packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-              modules = [
-                dream2nix.modules.dream2nix.WIP-python-pyproject
-                ({ config, ... }: {
-                  deps = {nixpkgs, ...}: {
-                    python = nixpkgs.python312;
-                    poetry = nixpkgs.poetry;
-                    patchelf = nixpkgs.patchelf;
-                  };
-
-                  public.pythonWithEnv = config.deps.python.withPackages (_: config.mkDerivation.propagatedBuildInputs);
-                  mkDerivation.src = ./jinko_env;
-                  paths.projectRoot = ./.;
-                  paths.package = "jinko_env";
-                  pip.overrides.kaleido.mkDerivation.postFixup = ''
-                    sed -i -e "s@/bin/bash@/bin/sh@" $out/lib/python*/site-packages/kaleido/executable/kaleido
-                    ${config.deps.patchelf}/bin/patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/lib/python*/site-packages/kaleido/executable/bin/kaleido
-                  '';
-                  # Not sure how to properly build a folder, so we turn
-                  # it into a whl file which is the known case
-                  pip.overrides.jinko-sdk.mkDerivation.src = let
-                    pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
-                    filename = "jinko_sdk-${pyproject.tool.poetry.version}-py${config.deps.python.sourceVersion.major}-none-any.whl";
-                  in pkgs.runCommand filename {} ''
-                    HOME=$(pwd)
-                    cd ${./.}
-                    ${config.deps.poetry}/bin/poetry build -o $HOME/dist
-                    cp $HOME/dist/${filename} $out
-                  '';
-                })
-              ];
-            };
+            jinkoEnv = mkJinkoEnv ./jinko_env "jinko_env";
+            jinkoEnvBig = mkJinkoEnv ./jinko_env_big "jinko_env_big";
           };
           # Default shell with only poetry installed
           devShells = {
@@ -94,6 +104,12 @@
               buildInputs = [
                 pkgs.bashInteractive
                 self.packages.${system}.jinkoEnv.pythonWithEnv 
+              ];
+            };
+            jinkoEnvBig = pkgs.mkShell {
+              buildInputs = [
+                pkgs.bashInteractive
+                self.packages.${system}.jinkoEnvBig.pythonWithEnv 
               ];
             };
           };
